ec9685832ca17aea47859bd11fd0c75969cdb808
vaduz <vaduzschool@gmail.com>
2020-10-02 14:27:13 +0200

    r

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://express-crc-git-node-test.apps-crc.testing`)
+app.listen(port, 'express-crc-git-node-test.apps-crc.testing', () => {
+  console.log(`Example app listening at http://express-crc-git-node-test.apps-crc.testing${port}`)
 })
