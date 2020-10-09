b4d52ed2d4a8b872dc1ba698672b00f541e5a2f3
vaduz <vaduz@core.lan>
2020-10-09 21:55:10 +0200

    Change port and remove ip

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 80
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, '127.0.0.1', () => {
-  console.log(`Example app listening at http://express-crc-git-node-test.apps-crc.testing${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening at localhost:${port}`)
 })
